edd226997f6cd40087071187a4dced63febfe08c
Ananya Tripathi <89539818+Anan-11@users.noreply.github.com>
2022-08-21 17:35:31 +0530

    Create Math.round.java

---
+++ Math.round.java
@@ -0,0 +1,10 @@
+public class Math_round_use_program {
+    public static void main(String[] args) {
+        System.out.println(Math.round(12.893));
+        //Round off the no. to 2 decimal places;
+
+        System.out.println(Math.round(12.895*Math.pow(10,2))/(Math.pow(10,2)));
+    }
+}
+
+//Math.round():return the decimal value(float or double) to integer
